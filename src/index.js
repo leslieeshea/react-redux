5bc72ca623d864ce7052631d22c1ffd9931a7a85
Leslie Shea <leslieyshea@gmail.com>
2019-05-22 14:38:10 -0700

    can add a drink

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import { createStore } from 'redux';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+const initialState = {
+  drink: null,
+  chips: null,
+  sandwich: null
+};
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'ADD_DRINK':
+      return { ...state, drink: action.payload };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.dispatch({
+  type: 'ADD_DRINK',
+  payload: 'Purple Fanta'
+});
+
+console.log('added drink', store.getState());
