537615f983560c8cbbadb65a777aee99cee9e454
Leslie Shea <leslieyshea@gmail.com>
2019-05-22 14:46:11 -0700

    can remove a drink

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ function reducer(state = initialState, action) {
       return { ...state, chips: action.payload };
     case 'ADD_SANDWICH':
       return { ...state, sandwich: action.payload };
+    case 'REMOVE_DRINK':
+      return { ...state, drink: null };
     default:
       return state;
   }
@@ -41,3 +43,10 @@ store.dispatch({
 });
 
 console.log('added sandwich', store.getState());
+
+store.dispatch({
+  type: 'REMOVE_DRINK',
+  payload: 'Purple Fanta'
+});
+
+console.log('removed drink', store.getState());
