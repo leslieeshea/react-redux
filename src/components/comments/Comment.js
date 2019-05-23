efd2236c3779ef60837b1dc90dbd38a1947ee5bc
Leslie Shea <leslieyshea@gmail.com>
2019-05-23 16:20:41 -0700

    can create a comment

---
+++ src/components/comments/Comment.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Comment({ id, comment, deleteComment }) {
+  return (
+    <section>
+      <p>{comment}</p>
+      <button onClick={deleteComment.bind(null, id)}>Delete Comment</button>
+    </section>
+  );
+}
+
+Comment.propTypes = {
+  id: PropTypes.number.isRequired,
+  comment: PropTypes.string.isRequired,
+  deleteComment: PropTypes.func.isRequired
+};
+
+export default Comment;
