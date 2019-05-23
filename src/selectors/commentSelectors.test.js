229bb0e875e4958eac0df1e9da80613bbe43880c
Leslie Shea <leslieyshea@gmail.com>
2019-05-23 14:28:09 -0700

    can get all comments by post id test passing

---
+++ src/selectors/commentSelectors.test.js
@@ -0,0 +1,13 @@
+import { getComments } from './commentSelectors';
+
+describe('Comment Selectors', () => {
+  it('gets all comments by post id', () => {
+    const state = {
+      comments: {
+        1: ['first comment', 'second comment']
+      }
+    };
+
+    expect(getComments(state, 1)).toEqual(['first comment', 'second comment']);
+  });
+});
