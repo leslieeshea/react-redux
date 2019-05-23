4766b3c90a70b40c1173bed1e6f354f5d5a33845
Leslie Shea <leslieyshea@gmail.com>
2019-05-23 16:13:20 -0700

    created AllPosts container

--- src/containers/posts/CreatePost.js
+++ src/containers/posts/CreatePost.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
-import { createPost } from '../../../actions/postActions';
-import PostForm from '../../posts/PostForm';
+import { createPost } from '../../actions/postActions';
+import PostForm from '../../components/posts/PostForm';
 
 const mapDispatchToProps = dispatch => ({
   onSubmit(title, body) {
