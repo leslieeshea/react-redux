434f65346d345f502c73287e570242c0f89c91a7
Leslie Shea <leslieyshea@gmail.com>
2019-05-22 17:18:44 -0700

    can delete a post test passing

--- src/actions/postActions.test.js
+++ src/actions/postActions.test.js
@@ -12,4 +12,11 @@ describe('Post Actions', () => {
       payload: { title: 'post title', body: 'post body' }
     });
   });
+
+  it('can delete a post', () => {
+    expect(deletePost(1)).toEqual({
+      type: DELETE_POST,
+      payload: 1
+    });
+  });
 });
