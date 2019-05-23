6775e8bcaca949c8191888aaafaf90f6e3e196b2
Leslie Shea <leslieyshea@gmail.com>
2019-05-23 10:27:59 -0700

    handles the create comment action with existing comments test passing

--- src/reducers/commentReducer.test.js
+++ src/reducers/commentReducer.test.js
@@ -4,10 +4,21 @@ import {
 } from '../actions/commentActions';
 
 describe('Comment Reducer', () => {
-  it('handles the create comments post action', () => {
+  it('handles the create comments action', () => {
     const newState = commentReducer({}, createComment(1, 'comment comment'));
     expect(newState).toEqual({
       1: ['comment comment']
+    });
+  });
+
+  it('handles the create comments action with existing comments', () => {
+    const initialState = {
+      1: ['here is my comment']
+    };
+
+    const newState = commentReducer(initialState, createComment(1, 'another comment'));
+    expect(newState).toEqual({
+      1: ['here is my comment', 'another comment']
     });
   });
 });
