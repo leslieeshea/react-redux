a91152cdf9dfdf087cd339fdf764d9ab53502edf
Leslie Shea <leslieyshea@gmail.com>
2019-05-23 17:00:09 -0700

    updated index with provider

--- src/containers/comments/CreateComment.js
+++ src/containers/comments/CreateComment.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import createComment from '../../actions/commentActions';
+import { createComment } from '../../actions/commentActions';
 import CommentForm from '../../components/comments/CommentForm';
 
 const mapDispatchToProps = (dispatch, props) => ({
